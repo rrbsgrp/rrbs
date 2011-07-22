242059584b2a0ae97f8c39ae0831831cbbf05b35
Zor Abad <zorinamaika@gmail.com>
2011-07-22 13:26:15 +0800

    Fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrbs::Application.config.secret_token = '37451c0aefc04772d3da03141fb38fc04763e50759f307b295844583996feada8c5d231d8ceda1daad3fb2f978909d536c80c20d226e3549b0b9edb6f41ee308'
